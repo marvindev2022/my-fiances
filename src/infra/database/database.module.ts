e6e13f02021316f0b2455802e3f54719b5e7139f
marvindev2022 <mavirolero@gmail.com>
2024-04-07 17:26:41 -0300

    feat: add database module and user repository for Prisma integration

---
+++ src/infra/database/database.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { UsersDatabaseModule } from './prisma/repositories/prisma-user-database.module';
+
+@Module({
+  imports: [
+    UsersDatabaseModule,
+    
+  ],
+})
+export class DatabaseModule {}
