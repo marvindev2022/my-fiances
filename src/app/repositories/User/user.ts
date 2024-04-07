5df41492cd24b33dc13bcbe1fff70a3b6bfdc3c0
marvindev2022 <mavirolero@gmail.com>
2024-04-07 17:25:59 -0300

    feat: add new files and interfaces for HTTP, crypto, email templates, and error handling

---
+++ src/app/repositories/User/user.ts
@@ -0,0 +1,34 @@
+import { User } from '@domain/User/User';
+import { DeleteUserDTO } from '@infra/http/dtos/User/deleteUser.dto';
+import { EditUserDTO } from '@infra/http/dtos/User/editUser.dto';
+import { FindedUserDTO } from '@infra/http/dtos/User/findedUser.dto';
+import { UserLoginDTO } from '@infra/http/dtos/User/login.dto';
+import { NotFoundException } from '@nestjs/common';
+
+
+export abstract class UserRepository {
+  abstract register(user: User): Promise<string>;
+
+  abstract login(account: UserLoginDTO): Promise<any | Error>;
+
+  abstract edit(
+    userId: string,
+    account: EditUserDTO,
+    photoFile?: Express.Multer.File,
+  ): Promise<void | Error>;
+
+  abstract findUserById(userId: string): Promise<User>;
+
+  abstract saveImage(id:string,photoUrl:string): Promise<any>;
+  
+  abstract updatePassword(
+    userId: string,
+    newPassword: string,
+  ): Promise<User | boolean | Error>;
+
+  abstract findByEmail(
+    email: string,
+  ): Promise<FindedUserDTO | NotFoundException>;
+
+  abstract deleteUser(request: DeleteUserDTO, id: string): Promise<void>;
+}
